6001f3697584fc872e5006bc796eefddc10c03f5
alexLauk <aleksandr.romanko.dev@gmail.com>
2021-01-15 21:14:31 +0200

    created new method in FighterService

--- src/javascript/services/fightersService.js
+++ src/javascript/services/fightersService.js
@@ -13,8 +13,15 @@ class FighterService {
   }
 
   async getFighterDetails(id) {
-    // todo: implement this method
-    // endpoint - `details/fighter/${id}.json`;
+    //---------------my code------------------------------//
+    try {
+      const endpoint = `details/fighter/${id}.json`;
+      const apiResult = await callApi(endpoint, 'GET');
+      return apiResult;
+    } catch (error) {
+      throw error;
+    }
+    //--------------------------------------------------//
   }
 }
 
