a1b0d567ca1efe97f453e6c5684117ea8159bfa8
alexLauk <aleksandr.romanko.dev@gmail.com>
2021-01-15 21:24:22 +0200

    modified function createFighterPreview

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -7,17 +7,30 @@ export function createFighterPreview(fighter, position) {
     className: `fighter-preview___root ${positionClassName}`,
   });
 
-  // todo: show fighter info (image, name, health, etc.)
+  //----------------my code------------------------------//
+  const card = createElement({tagName:'div', className: 'fighter-preview___card'})
+  const fitherImage = createFighterImage(fighter)
+  const cardContent = fighter ? Object.entries(fighter)
+                                      .filter(i => !/source|_id/g.test(i[0]))
+                                      .reduce((prev, curr) => {
+                                          prev += `<p>${curr[0]}: ${curr[1]}<p>`
+                                          return prev
+                                      },'<div class="fighter-info">') : ''
+
+  card.insertAdjacentHTML('beforeend', cardContent)
+  card.prepend(fitherImage)
+  fighterElement.append(card)
+  //------------------------------------------------//
 
   return fighterElement;
 }
 
 export function createFighterImage(fighter) {
   const { source, name } = fighter;
-  const attributes = { 
-    src: source, 
+  const attributes = {
+    src: source,
     title: name,
-    alt: name 
+    alt: name
   };
   const imgElement = createElement({
     tagName: 'img',
